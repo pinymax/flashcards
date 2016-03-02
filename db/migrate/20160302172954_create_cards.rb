c357aa63aeb31ba43ed64b8ecb7e937fbc361aee
Max Obushenko <max.obushenko@gmail.com>
2016-03-02 21:06:15 +0200

    Migrated Card model

--- db/migrate/20160302172954_create_cards.rb
+++ db/migrate/20160302172954_create_cards.rb
@@ -1,7 +1,9 @@
 class CreateCards < ActiveRecord::Migration
   def change
     create_table :cards do |t|
-
+      t.text     :original_text
+      t.text     :translated_text
+      t.datetime :review_date
       t.timestamps null: false
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160302171245) do
+ActiveRecord::Schema.define(version: 20160302172954) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cards", force: :cascade do |t|
+    t.text     "original_text"
+    t.text     "translated_text"
+    t.datetime "review_date"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.text     "text"
