000d4a7e82090a2b475d2cf69ab89aad1ac4183b
Max Obushenko <max.obushenko@gmail.com>
2016-03-03 17:13:30 +0200

    Created others RESTfull controllers and views

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,4 +1,7 @@
 class CardsController < ApplicationController
+
+  before_action :find_card, only: [:show, :edit, :update, :destroy]
+
   def index
     @cards = Card.all
   end
@@ -8,28 +11,42 @@ class CardsController < ApplicationController
   end
 
   def create
-    @card = Card.new(cards_params)
+    @card = Card.create(cards_params)
     @card.review_date = Time.now+3.days
     @card.save
-    redirect_to @card
+    if @card.errors.empty?
+      redirect_to @card
+    else
+      render "new"
+    end
   end
 
   def show
-    @card = Card.find(params[:id])
   end
 
   def edit
   end
 
   def update
+    @card.update_attributes(cards_params)
+    if @card.errors.empty?
+      redirect_to @card
+    else
+      render "new"
+    end
   end
 
   def destroy
+    @card.destroy
   end
 
   private
 
   def cards_params
     params.require(:card).permit(:original_text, :translated_text)
+  end
+
+  def find_card
+    @card = Card.find(params[:id])
   end
 end
