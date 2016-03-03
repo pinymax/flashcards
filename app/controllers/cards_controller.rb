6c8c1d45665e8ebdd2213b47c4582c8a11e32b14
Max Obushenko <max.obushenko@gmail.com>
2016-03-03 13:05:58 +0200

    Added RESTfull actions to controller and created a new's view

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,4 +2,34 @@ class CardsController < ApplicationController
   def index
     @cards = Card.all
   end
+
+  def new
+    @card = Card.new
+  end
+
+  def create
+    @card = Card.new(cards_params)
+    @card.review_date = Time.now+3.days
+    @card.save
+    redirect_to @card
+  end
+
+  def show
+    @card = Card.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def cards_params
+    params.require(:card).permit(:original_text, :translated_text)
+  end
 end
