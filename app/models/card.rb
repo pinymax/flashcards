667354ef428d4c9a51abfebe245ba9bfd52047e1
Max Obushenko <max.obushenko@gmail.com>
2016-03-03 20:17:42 +0200

    Add Validation

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,10 +1,20 @@
 class Card < ActiveRecord::Base
-before_save :date_set
+  validates_presence_of  :original_text, :translated_text
+  validate :check
 
 
 
-  def date_set
-    self.review_date = Time.now+3.days
-  end
+
+private
+    def date_set
+      self.review_date = Time.now+3.days
+    end
+
+    def check
+     if original_text == self.translated_text
+       errors.add(:translated_text, "Содержимое полей не может быть одинаковым!")
+     end
+    end
+
 
 end
