0cbcb48a00fa9eac2c181aa4615f6c8ce5e29260
Max Obushenko <max.obushenko@gmail.com>
2016-03-06 20:00:22 +0200

    Fixed, if translate is right, it updates review_date and when we reload page, we see another card

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -13,12 +13,15 @@ class CardsController < ApplicationController
 
   def index_train
     random_card()
+  unless @train.nil?
     if params[:transl] == @train.translated_text
        flash[:notice] = "Good"
+       @train.review_date = Time.now + 3.days
+       @train.save
        redirect_to root_url
-     end 
+     end
   end
-
+end 
 
 
 
