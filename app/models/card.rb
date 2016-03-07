29d5e5f9d7c11da89c5775ca45c1b05b189785bc
Max Obushenko <max.obushenko@gmail.com>
2016-03-07 12:04:57 +0200

    Style vialation

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,9 +2,7 @@ class Card < ActiveRecord::Base
   validates :original_text, :translated_text, presence: true
   validate  :check_if_fields_uniq_between_themselfs
   before_save :date_set
-  scope :checktime, ->  { Card.where("review_date <= ?", Date.today) }
-
-
+  scope :checktime, -> { Card.where("review_date <= ?", Date.today) }
 
 private
 
@@ -26,11 +24,8 @@ private
 
     def self.traing(train)
       if train
-         train.review_date = Time.now + 3.days
-         train.save
+        train.review_date = Time.now + 3.days
+        train.save
       end
     end
-
-
-
 end
