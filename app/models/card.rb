a9b7315eed177a41e5e968bcd02fb1874c0fa32e
Max Obushenko <max.obushenko@gmail.com>
2016-03-07 12:10:19 +0200

    Fixed Houndci vialation

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,7 +1,7 @@
 class Card < ActiveRecord::Base
   validates :original_text, :translated_text, presence: true
   validate  :check_if_fields_uniq_between_themselfs
-  before_save :date_set
+#  before_save :date_set
   scope :checktime, -> { Card.where("review_date <= ?", Date.today) }
 
 private
@@ -17,12 +17,14 @@ private
     end
 
     def self.random_card
+      private
       offset = rand(checktime.count)
       train = checktime.offset(offset).first
       train
     end
 
     def self.traing(train)
+      private
       if train
         train.review_date = Time.now + 3.days
         train.save
