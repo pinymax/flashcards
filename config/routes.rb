15694718d9b3c1703f81cf3663f9e1065c982e95
Max Obushenko <max.obushenko@gmail.com>
2016-03-07 14:58:15 +0200

    Hound fix x4

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   root "cards#index_train"
-  
   post "cards" => "cards#index_train"
   # You can have the root of your site routed with "root"
 
