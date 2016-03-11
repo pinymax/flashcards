5ad7067705188aff209571b64afc74b6e6df7c3f
Max Obushenko <max.obushenko@gmail.com>
2016-03-12 00:24:33 +0200

    Fixed HoundCI style's issues

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,23 +1,23 @@
-require 'rails_helper'
+require "rails_helper"
 
 describe Card do
   it "Checks the compare method" do
-    card = Card.new(original_text: "hause",translated_text: "battle")
+    card = Card.new(original_text: "hause", translated_text: "battle")
     expect(card.checker("hAlel")).to be false
   end
 
   it "Checks the compere method with bad argument" do
-    card = Card.new(original_text: "hause",translated_text: "battle")
+    card = Card.new(original_text: "hause", translated_text: "battle")
     expect(card.checker("   BAttlE")).to be false
   end
 
   it "Checks the compere method with good argument" do
-    card = Card.new(original_text: "hause",translated_text: "battle")
+    card = Card.new(original_text: "hause", translated_text: "battle")
     expect(card.checker("battle")).to be true
   end
 
   it "Checks the date setter" do
-    card = Card.create(original_text:"house", translated_text: "ha")
+    card = Card.create(original_text: "house", translated_text: "ha")
     expect(card.review_date == Date.today + 3.days).to be true
   end
 
