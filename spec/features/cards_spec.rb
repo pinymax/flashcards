9e39b8389a2cec07de92855e357afbfb745bdd1e
Max Obushenko <max.obushenko@gmail.com>
2016-03-16 20:41:27 +0200

    Style fix 2

--- spec/features/cards_spec.rb
+++ spec/features/cards_spec.rb
@@ -6,21 +6,22 @@ describe "train", type: :feature do
     before do
       card.update(review_date:  Date.today)
     end
-      it "When user input right translate" do
-        visit root_path
-        puts page.body
-        fill_in("translate", :with => "Дом")
-        click_button("Отправить")
-        expect(page).to have_content ("В точку!")
-      end
 
-      it "When user input wrong translate" do
-        card.update_attribute :review_date, Date.today
-        visit root_path
-        puts page.body
-        fill_in("translate", :with => "asSds")
-        click_button("Отправить")
-        expect(page).to have_content ("Неверный перевод")
-      end
+    it "When user input right translate" do
+      visit root_path
+      puts page.body
+      fill_in("translate", with: "Дом")
+      click_button("Отправить")
+      expect(page).to have_content "В точку!"
+    end
+
+    it "When user input wrong translate" do
+      card.update_attribute :review_date, Date.today
+      visit root_path
+      puts page.body
+      fill_in("translate", with: "asSds")
+      click_button("Отправить")
+      expect(page).to have_content "Неверный перевод"
+    end
   end
 end
