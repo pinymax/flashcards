6b136143542dc62fad53e23871cf19c1c7ab2dba
Max Obushenko <max.obushenko@gmail.com>
2016-04-09 22:20:38 +0300

    A LOT OF TRASH

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def not_authenticated
-  redirect_to login_path
+    redirect_to login_path
   end
 end
