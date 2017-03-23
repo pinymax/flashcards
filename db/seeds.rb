83d3d6d2aa85ac4f50b3caa6ca23d8e3a81c528c
Max Obushenko <max.obushenko@gmail.com>
2017-03-23 11:18:10 +0200

    added gem for production

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,5 +8,5 @@ origin = doc.css(".bigLetter")
 transl = doc.css(".bigLetter+ td")
 
 origin.each_with_index do |val, index|
-  Card.create({"original_text" => "#{val.text}", "translated_text" => "#{transl[index].text}" })
+  Card.create({"original_text" => "#{val.text}", "translated_text" => "#{transl[index].text}", "user_id" => User.last.id })
 end
